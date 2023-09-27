70173821ee040bb6cb1367b4c340d1d55bc151cf
Alisson <arteaga10alisson@gmail.com>
2023-09-27 13:52:11 -0500

    update paswword encrypted to users

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert INTO usuarios (username, password, enabled, nombre, apellido, email) values('andres', '12345', 1, 'Andres', 'Guzman', 'profesor@gmail.com');
-insert INTO usuarios (username, password, enabled, nombre, apellido, email) values('admin', '12345', 1, 'Jhon', 'Doe', 'jhon1034@gmail.com');
+insert INTO usuarios (username, password, enabled, nombre, apellido, email) values('andres', '$2a$10$xiPIgZCoPD.ZMCcGrpPtp.hFiNcwJ1eq.cx3SaT9Ar9G5wzQiOFmO', 1, 'Andres', 'Guzman', 'profesor@gmail.com');
+insert INTO usuarios (username, password, enabled, nombre, apellido, email) values('admin', '$2a$10$IyPWl3ZbsjQ7Fqt1GIZiWuBsVv.6H.D1msy1MJeW/aWJ2mk..I2ee', 1, 'Jhon', 'Doe', 'jhon1034@gmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
